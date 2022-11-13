08a31d51376177c121e28c7c1ce78b3e0c0abbd0
RattanLegs <112847410+RattenLegs@users.noreply.github.com>
2022-11-13 23:51:24 +0900

    Add files via upload

--- WhatsupJava/chap05/DoWhileTest.java
+++ WhatsupJava/chap05/DoWhileTest.java
@@ -3,7 +3,21 @@ package chap05;
 public class DoWhileTest {
 
 	public static void main(String[] args) {
-
+		int i,j;
+		
+		i=10;
+		
+		do {
+			j=0;
+			do {
+				System.out.print("*");
+				j++; //j값 증가
+			} while(j<i); //j값이 i값보다 작으면 반복
+				System.out.println();
+				i--;
+			
+		} while(i>0); //i값이 0보다 크면 반복
 	}
 
 }
+ 
---
+++ WhatsupJava/chap05/ForTest.java
@@ -0,0 +1,12 @@
+package chap05;
+
+public class ForTest {
+
+	public static void main(String[] args) {
+		for(int i=10; i>0; i--) {
+			System.out.print("*");
+		}
+		
+	}
+
+}
